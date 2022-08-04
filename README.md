74a7b9ce5566f67980fc636f9cefe81f718bb27a
Thomas Hervé <thomas.herve@datadoghq.com>
2022-08-04 10:11:44 +0200

    Load config

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Synthetics CDK demo
 
 Showcase how to use CDKTF to create Datadog synthetics tests.
+
+```shell
+$ poetry run python -m synthetics_cdk config.yml
+```
--- src/synthetics_cdk/cli.py
+++ src/synthetics_cdk/cli.py
@@ -1,4 +1,13 @@
+import pathlib
+import yaml
+
+import click
 
 
-def cli():
-    pass
+@click.command()
+@click.argument(
+    "config",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, path_type=pathlib.Path))
+def cli(config):
+    with config.open() as config_file:
+        config_data = yaml.safe_load(config_file)
